2bb865319445821fb03f973c84e04538df66f07d
ChoiKyeungJin <55359778+ChoiKyeungJin@users.noreply.github.com>
2020-09-06 21:37:42 +0900

    필요한 파일들

---
+++ include/static_avoidance.h
@@ -0,0 +1,9 @@
+#include ros/ros.h
+#include cmath
+
+
+from obstacle_detector.msg import Obstacles
+from obstacle_detector.msg import SegmentObstacle 
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import PointStamped
+from ackermann_msgs.msg import AckermannDriveStamped
